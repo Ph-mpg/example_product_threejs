b93923a94d26757df8483af277cd320921856c27
Philippe <92359903+phylxs@users.noreply.github.com>
2022-05-06 00:49:02 +0200

    commerce avec three

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Commerce from "@chec/commerce.js";
+import Item from './components/Item'
+import './App.css'
 
-function App() {
+const commerce = new Commerce(
+  "pk_test_41976d36632e5b8d334f5c36edc78bfe60e30b428ed61"
+);
+
+
+
+export default function App() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    commerce.products.list().then((res) => {
+      setProducts(res.data);
+    });
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      {products.map((product) => (
+        <Item key={product.id} {...product} />
+      ))}
     </div>
+    
+
   );
 }
 
-export default App;
+
